de8e89221b376412fc6f74bb6401b7e3420f9bc1
Rub21 <ruben@mapbox.com>
2015-10-26 10:22:20 -0500

    change

--- install.sh
+++ install.sh
@@ -21,5 +21,3 @@ wget -O - http://m.m.i24.cc/osmfilter.c |cc -x c - -O3 -o osmfilter
 sudo cp osmfilter /usr/bin/
 wget -O - http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -O3 -o osmconvert
 sudo cp osmconvert /usr/bin/
-
-psql --host=localhost --port=5432 --username=postgres --dbname=dbosm -f query/tables.sql
