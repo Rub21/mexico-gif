8f75a7052a830f452f2a29f029d1dd7795e69acd
Rub21 <ruben@mapbox.com>
2015-10-29 14:34:53 -0500

    update

--- buildosm.sh
+++ buildosm.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-url="https://s3.amazonaws.com/osm-changesets/day/000/001/"
+#url="https://s3.amazonaws.com/osm-changesets/day/000/001/"
 #url="http://planet.osm.org/redaction-period/day-replicate/000/000/"
 ##per hour
-#url="https://s3.amazonaws.com/osm-changesets/hour/000/027/"
+url="https://s3.amazonaws.com/osm-changesets/hour/000/027/"
 sed 's/@//g' $4 > temp
 sed 's/,/,/g' temp > u
 for i in $(seq $1 $2)
-do	       
+do	
+    echo ${url}$i.osc.gz       
     if (($i<10)); then
        curl ${url}00$i.osc.gz -o "$i.osc.gz"
     fi
@@ -29,16 +30,22 @@ do
       IFS="," read -ra STR_ARRAY <<< "$users"
       for j in "${STR_ARRAY[@]}"
       do
-          osmfilter $i.osm --keep="@user=$j" -o=$i-$j.osm
+          osmfilter $i.osm --keep=\"@user=$j\" -o=$i-$j.osm
       done
       rm $i.osm
       osmconvert $i-*.osm -o=$i.osm
       rm $i-*.osm
     fi
     rm $i.osc.gz
+
+    if(($i == $1)); then
+      osmconvert $i.osm --complete-ways -o=main.osm
+    else
+      updateosm $i.osm main.osm
+    fi
     echo "Process completed $i"
 done
-osmconvert *.osm -o=osm.osm
-bzip2 osm.osm
-rm *.osm
-rm u
+# osmconvert *.osm -o=osm.osm
+# bzip2 osm.osm
+# rm *.osm
+# rm u
