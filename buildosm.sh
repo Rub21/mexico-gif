05f21c05082d96469426eb3e51a8c155e3fd0922
Rub21 <ruben@mapbox.com>
2016-04-06 08:00:43 -0500

    processing maps for mexico import

--- buildosm.sh
+++ buildosm.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 wget http://planet.openstreetmap.org/planet/2015/planet-150928.osm.bz2
-osmconvert planet-150928.osm.bz2 -B=boundary/mexico.poly -o=mexico.osm
+bzip2 -d planet-150928.osm.bz2 
+
+osmconvert planet-150928.osm -B=boundary/mexico.poly -o=mexico.osm
+
+
+wget http://download.geofabrik.de/north-america/mexico-latest.osm.bz2
+bzip2 -d mexico-latest.osm.bz2 
+
+./osmfilter mexico-latest.osm --keep=  --keep-relations="boundary=administrative and admin_level=6" -o=mexico-bounduary.osm
+osmfilter mexico-bounduary.osm --keep=@user=mexico_boundaries_import -o=mexico_boundaries_import.osm
+
+osmconvert mexico_boundaries_import.osm -o=mexico_boundaries_import.osm.pbf
+osmium cat --object-type=relation --output=mexico_boundaries_import.osm.opl mexico_boundaries_import.osm.pbf
+
+
+osmium getid --add-referenced --id-file=mexico_boundaries_import.osm.opl --output=boundaries_import.osm.pbf mexico_boundaries_import.osm.pbf
+minjur boundaries_import.osm.pbf > boundaries_import.geojson
+
--- install.sh
+++ install.sh
@@ -3,3 +3,30 @@ wget -O - http://m.m.i24.cc/osmfilter.c |cc -x c - -O3 -o osmfilter
 sudo cp osmfilter /usr/bin/
 wget -O - http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -O3 -o osmconvert
 sudo cp osmconvert /usr/bin/
+
+
+sudo apt-get install cmake cmake-curses-gui make libexpat1-dev zlib1g-dev libbz2-dev
+sudo apt-get install libsparsehash-dev libboost-dev libboost-program-options-dev libgdal-dev libgeos++-dev libproj-dev doxygen graphviz
+git clone https://github.com/osmcode/libosmium.git
+cd libosmium/
+mkdir build
+cd build
+cmake ..
+make
+cd /
+
+git clone https://github.com/mapbox/minjur.git
+cd minjur/
+mkdir build
+cd build
+cmake ..
+make
+
+
+git clone https://github.com/osmcode/osmium-tool.git
+cd osmium-tool/
+mkdir build
+cd build
+cmake ..
+make
+
